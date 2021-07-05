7cb55f6755aa5d3c105b0342104dbd7e35f3cbe2
HecAllende <hectorallende@gmail.com>
2021-07-05 11:19:21 -0300

    probando 1

--- src/components/imagenInicio.js
+++ src/components/imagenInicio.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {graphql, useStaticQuery} from 'gatsby'
 import Img from 'gatsby-image'
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 
 
